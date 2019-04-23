da599053380f93ad552f62df516e04b59853c4fa
Oleg Yushkevich <alehyushkevich@gmail.com>
2019-04-23 15:45:24 +0300

    Interval commit with part of code of portfolio eval

--- Feature_ext.py
+++ Feature_ext.py
@@ -1,4 +1,30 @@
-import pandas as pd 
+import pandas as pd
+import numpy 
+
+#Получаем Данные за прошлый год и за текущий(месячный датафрейм)
+        #Сортируем компании в YearDataFrame по BE/MC
+        #Делим компании на value , medium , growth в соотношении 30%/40%/30%
+        #Так же делим по капитализации на small , Big(70-30) (это будет в исходных данных)
+        #Выбираем компании из любой категории (например value small)
+        #Взвешиваем компании по капитализации(????)
+        #Считаем доли капитализации компании к общей капитализации категории
+        #Считаем доходности порфтеля за июнь
+        #Ребалансируем веса как w_(t-1) * (1 + price_return)
+        #итерируемся по нашей бд по всем месяцам, когда доходим до июня - меняем год(в базе за месяц нужны данные
+        # от июня до июня)
+
+# def BuildPortfolio(YearDataFrame  , MonthDataFrame ):
+#        YearDataFrame.sort_values(by = ['bookToMktcap'] , ascending = True)
+#        GrowthQuantile = YearDataFrame.quantile(q=0.3)['bookToMktcap']
+#        MidQuantile = YearDataFrame.quantile(q=0.7)['bookToMktcap']
+
+#        GrowthDataFrame = YearDataFrame[YearDataFrame['bookToMktcap'] < GrowthQuantile]
+#        MidDataFrame = YearDataFrame[YearDataFrame['bookToMktcap'] < MidQuantile and YearDataFrame['bookToMktcap'] >= GrowthQuantile]
+#        ValueDataFrame = YearDataFrame[YearDataFrame['bookToMktcap'] >= MidQuantile]
+
+#        SmallValueDataFrame = ValueDataFrame[ValueDataFrame['Size']] # ????????
+
+
 
 
 
@@ -11,30 +37,27 @@ YearDataFrame = pd.read_sas('c:\\Data\\allyear.sas7bdat' , chunksize= None , ite
 YearDataFrame = YearDataFrame[['year' ,'ws_id' , 'book' , 'mktcap_Dec' , 'cap_size']]
 
 YearDataFrame.loc[: , 'bookToMktcap'] = YearDataFrame.apply(lambda row : row['book'] / row['mktcap_Dec'] , axis=1)
-YearDataFrame.loc[: , 'Value'] = YearDataFrame.apply(lambda row:  'Growth ' if (row['bookToMktcap'] <1) else 'Value' , axis=1)
+#YearDataFrame.loc[: , 'Value'] = YearDataFrame.apply(lambda row:  'Growth' if (row['bookToMktcap'] <1) else 'Value' , axis=1)
 YearDataFrame = YearDataFrame.drop(YearDataFrame[YearDataFrame.year < 1986].index)
 
-item = 0
-for index ,row in YearDataFrame.iterrows() :
-    if(row['Value'] == "Value"):
-        item=item+1
+
+del YearDataFrame['cap_size']
+
+
+CapitalMedian = YearDataFrame.quantile(0.5)
+print(CapitalMedian)
+#YearDataFrame.loc[: , 'cap_size'] = YearDataFrame.apply(lambda row : 'Small' if([row['mktcap_Dec'] <CapitalMedian]) else 'Large' , axis=1)
+        
 
 
 
 
-print(item)
-print(len(YearDataFrame) - item)
-print('________________')
+# print(YearDataFrame.shape)
 
+# print(YearDataFrame.head())
 
-
-
-print(YearDataFrame.shape)
-
-print(YearDataFrame.head())
-
-print(MonthDataFrame.shape)
-print(MonthDataFrame.head())
+# print(MonthDataFrame.shape)
+# print(MonthDataFrame.head())
 
 #book - ������������� �������� - ������� ������
 
